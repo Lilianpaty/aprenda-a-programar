24bf20a0988456a10354561f8f294ed589645396
Lilian Patricia <lilianpatricia10@yahoo.com.br>
2022-08-27 23:27:17 -0300

    dados pessoais

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,8 @@ let email
 let prifissao 
 let idade 
 let lugarqueroconhecer 
+nome = "lilian Patricia dias de souza" 
+email = "lilianpatricia10@yahoo.com.br"
+profissao = "empreendedora"
+idade = "38"
+lugarquequeroconhecer = "jericoacoara"
