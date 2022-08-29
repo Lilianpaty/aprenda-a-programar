c804440a96cbfd45cfb6a7464541236c9e4d130b
Lilian Patricia <lilianpatricia10@yahoo.com.br>
2022-08-29 00:43:53 -0300

    Revert "codigo alan turing"
    This reverts commit ad3119225fab9b7e9cfdfa952f742daddc7b06fc.

--- tarefa-dia-2/pratica-3.js
+++ tarefa-dia-2/pratica-3.js
@@ -5,7 +5,7 @@ console.log(tamanho)
 let codigo = "alanturing"
 let valorresultante = 6110
 
-if (valorresultante >= 6110) {
+if (valorresultante <= 6110) {
     console.log("Maravilha! O código tem menos ou é igual a 6110 caracteres! Mãos à obra! Decifrem o código o quanto antes que vocês vão conseguir derrotá-los! :D")
 } else {
     console.log("Perdão, Alan Turing. Vocês não conseguirão derrotar os nazistas. :'(")
